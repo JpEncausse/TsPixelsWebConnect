065ad566a7b7bc6eff4c22b2e69870a2dca1c91f
Jean-Philippe Encausse <Jp@encausse.net>
2022-10-03 19:02:24 +0200

    It works !

--- demo/demo.js
+++ demo/demo.js
@@ -3,6 +3,25 @@ const connect = async (Pixel) => {
     console.log("Connecting...");
     await pixel.connect();
     console.log("Done");
+
+    const rollState = await pixel.getRollState();
+    console.log(`=> roll state: ${rollState.state}, face ${rollState.faceIndex}`);
+
+    const battery = await pixel.getBatteryLevel();
+    console.log(`=> battery: ${battery.level}, ${battery.voltage}v`);
+
+    const rssi = await pixel.getRssi();
+    console.log(`=> rssi: ${rssi}`);
+
+    const blink = await pixel.blink(Color.red);
+    console.log(`=> blink: ${blink}`);
+
+    // Add listener to get notified when the Pixel roll state changes
+    pixel.addEventListener("messageRollState", (ev) => {
+    // Or: pixel.addMessageListener(MessageTypeValues.RollState, (ev: CustomEvent<MessageOrType>) => {
+        const msg = ev.detail;
+        console.log(`=> roll state: ${msg.state}, face ${msg.faceIndex + 1}`);
+    });
 }
 
 
@@ -11,4 +30,3 @@ window.requirejs(['/dist/umd/index.js'], (Pixel) => {
         connect(Pixel)
     });
 });
-
